89fe714dc24d9419611b9e4df4a2d60bb0a022fb
Nidhal Dalhoumi <113780364+Nidhaldal@users.noreply.github.com>
2026-09-15 11:34:34 +0100

    feat: allow Vercel frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://localhost:5173"],
+    allow_origins=["http://localhost:4200" ,"https://theexperience.vercel.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
